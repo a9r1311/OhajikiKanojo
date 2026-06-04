87a3d4087bccd36d4e793c78d3c2ce2292ee4982
a9r1311 <a9r1311@outlook.jp>
2026-06-04 15:08:19 +0900

    チュートリアル中途

---
+++ Assets/Script/Core/Other/CharacterMove.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+    [SerializeField] FollowTargetUI target;
+    public GameObject prince;
+    float moveSpd = 12f;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if(target.explaind)
+        {
+            transform.position += Vector3.forward * moveSpd * Time.deltaTime;
+            //Vector3 moveDirection = Vector3.forward;
+            //moveDirection.y = -2f;
+            //controller.Move(moveDirection * moveSpd * Time.deltaTime);
+        }
+    }
+}
