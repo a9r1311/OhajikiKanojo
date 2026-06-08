e4a88701e56c98fc12707b13cdca3defc09d10e8
a9r1311 <a9r1311@outlook.jp>
2026-06-08 09:24:18 +0900

    chargeエフェクト追加

--- Assets/Script/Enemy/EnemySpawnDirector.cs
+++ Assets/Script/Enemy/EnemySpawnDirector.cs
@@ -13,10 +13,10 @@ public class EnemySpawnDirector : MonoBehaviour
     private int poolCount = 4;  //とりあえずこの数ずつ用意しておく
     private List<GameObject> activeEnemies = new();  //現在アクティブな敵のリスト
 
-    private float initialSpawnInterval = 2f;  //スポーン間隔の初期値
+    private float initialSpawnInterval = 3.2f;  //スポーン間隔の初期値
     private float currentSpawnInterval;  //スポーン間隔
     private float minSpawnInterval = 0.35f;  //スポーン間隔の最小値
-    private float spawnIntervalDecreaseRate = 0.05f;  //スポーン間隔の減少率
+    private float spawnIntervalDecreaseRate = 0.086f;  //スポーン間隔の減少率
     private float spawnTimer = 0f;  //スポーンタイマー
 
     [SerializeField] private bool isLongDistanceSpawn = false;  //遠距離スポーンフラグ
@@ -76,7 +76,10 @@ public class EnemySpawnDirector : MonoBehaviour
 
             //スポーン間隔を徐々に短くする
             if ((currentSpawnInterval -= spawnIntervalDecreaseRate) < minSpawnInterval)
+            {
+                Debug.Log(2);
                 currentSpawnInterval = minSpawnInterval;
+            }
 
             if(spawnCounter >= increaseSpawnTypeInterval && spawnTypeCount < spawnEnemy.Count)
             {
